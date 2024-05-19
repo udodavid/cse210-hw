fedd4dba0e372358c22644043216d78724814a53
udodavid <davidudo00@gmail.com>
2024-05-19 06:18:27 +0100

    dev4

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Threading;
 
-class Program
+abstract class MindfulnessActivity
 {
-    static void Main(string[] args)
+    protected int duration;
+
+    public void StartActivity(string name, string description)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine($"Starting {name} Activity");
+        Console.WriteLine(description);
+        Console.Write("Enter the duration in seconds: ");
+        duration = int.Parse(Console.ReadLine());
+        PauseWithSpinner("Get ready to begin...");
     }
+
+    public void EndActivity(string name)
+    {
+        PauseWithSpinner("Great job!");
+        Console.WriteLine($"You have completed the {name} activity for {duration} seconds.");
+        PauseWithSpinner("Finishing up...");
+    }
+
+    protected void PauseWithSpinner(string message)
+    {
+        Console.Write(message);
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    public abstract void RunActivity();
 }
